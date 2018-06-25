ac478e21ea3bf6a04ade8aa6ea1013516d9699f4
Emmanuel Maldonado <emmanuel.maldonado@globant.com>
2018-06-25 13:31:07 -0500

    Add readfile function and test

--- src/fuzzySearch.js
+++ src/fuzzySearch.js
@@ -1,14 +1,22 @@
+import fsPromise from 'fs-readfile-promise';
+
 export default {
 
   parseCommandLine(mapArguments) {
     let json = {};
     let i = 3;
     const length = mapArguments.length;
-    for(i; i < length; i++){      
+    for (i; i < length; i++) {
       let keyValue = mapArguments[i].replace('{', '').replace('}', '').split(':');
       json[keyValue[0]] = keyValue[1];
     }
     return json;
+  },
+
+  readFile(pathFile) {
+    return fsPromise(pathFile).then((d) => {
+      return JSON.parse(d)
+    });
   },
 
 };
--- test/fuzzySearch.test.js
+++ test/fuzzySearch.test.js
@@ -1,8 +1,25 @@
 import fuzzySearch from '../src/fuzzySearch';
 
+const PATH_FILE = './assets/listTmp.txt';
+
 test('Must parse json from command line', () => {
   let jsonParam = '{name:Jhon Snow}'
   let argv = ['node', 'fuzzySearch.js', 'add', jsonParam]
   let jsonParsed = fuzzySearch.parseCommandLine(argv);
   expect(jsonParsed.name).toEqual('Jhon Snow')
+});
+
+test('Must read file', () => {
+  let expectedData = [
+    "Alberto Vera Padrón",
+    "Juan Antonio Perez",
+    "Rodolfo Juarez Fernandez"
+  ];
+
+  fuzzySearch.readFile(PATH_FILE).then((dataFromFile) => {
+    dataFromFile.forEach((data, i) => {
+      expect(data.name).toEqual(expectedData[i]);
+    });
+  });
+
 });
